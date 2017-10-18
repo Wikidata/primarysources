db6eb46264e0ea4d170c3c29cdc6ff92f4a55b36
Marco Fossati <fossati@spaziodati.eu>
2017-10-18 12:08:54 +0200

    remove back end version 1

--- backend/update1.sqlite.sql
+++
@@ -1,5 +0,0 @@
--- this is a migration script to update the database from the original schema to the new schema
--- with datasets and statement timestamps
-ALTER TABLE statement ADD COLUMN dataset VARCHAR(32) DEFAULT 'freebase';
-ALTER TABLE statement ADD COLUMN upload INTEGER DEFAULT 0;
-CREATE INDEX idx_stmst_dataset ON statement(dataset);
--- backend/update2.sqlite.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_stmt_subject_state_dataset ON statement(subject, state, dataset);
-DROP INDEX IF EXISTS idx_stmt_qid;
-CREATE INDEX IF NOT EXISTS idx_stmt_state_dataset_subject ON statement(state, dataset, subject);
-DROP INDEX IF EXISTS idx_stmt_state;
